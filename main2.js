82b60caace8f639f1f6a694e884a29f4e01fd659
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-20 06:22:11 +0900

    load board

---
+++ main2.js
@@ -0,0 +1,7 @@
+import { createBoard, playMove } from "./connect4.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const board = document.querySelector(".board");
+  createBoard(board);
+  console.log(`DOMContentLoaded`);
+});
