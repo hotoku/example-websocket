352a79db90e66161707088e7cdc0432f27337593
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-20 07:22:45 +0900

    sendMove

--- main2.js
+++ main2.js
@@ -1,7 +1,23 @@
 import { createBoard, playMove } from "./connect4.js";
 
+function sendMoves(board, websocket) {
+  board.addEventListener("click", ({ target }) => {
+    const column = target.dataset.column;
+    if (column === undefined) {
+      return;
+    }
+    const event = {
+      type: "play",
+      column: parseInt(column, 10),
+    };
+    websocket.send(JSON.stringify(event));
+  });
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   const board = document.querySelector(".board");
   createBoard(board);
+  const websocket = new WebSocket("ws://localhost:8002");
+  sendMoves(board, websocket);
   console.log(`DOMContentLoaded`);
 });
