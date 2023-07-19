a34e042cb4c63d5d0f4143293d14b5eda065fe06
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-20 08:01:35 +0900

    wip

--- main2.js
+++ main2.js
@@ -1,5 +1,28 @@
 import { createBoard, playMove } from "./connect4.js";
 
+function showMessage(message) {
+  setTimeout(() => alert(message), 50);
+}
+
+function receiveMessage(board, websocket) {
+  websocket.addEventListener("message", ({ data }) => {
+    const event = JSON.parse(data);
+    switch (event.type) {
+      case "play":
+        playMove(board, event.player, event.column, event.row);
+        break;
+      case "win":
+        showMessage(`Player ${event.player} wins!`);
+        break;
+      case "error":
+        showMessage(event.message);
+        break;
+      default:
+        throw new Error(`Unsupported event type: ${event.type}.`);
+    }
+  });
+}
+
 function sendMoves(board, websocket) {
   board.addEventListener("click", ({ target }) => {
     const column = target.dataset.column;
@@ -19,5 +42,6 @@ window.addEventListener("DOMContentLoaded", () => {
   createBoard(board);
   const websocket = new WebSocket("ws://localhost:8002");
   sendMoves(board, websocket);
+  receiveMessage(board, websocket);
   console.log(`DOMContentLoaded`);
 });
