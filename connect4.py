40893c0066bea4bf80e2a5442979e3b779274de7
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-07-21 04:33:34 +0900

    wip

--- connect4.py
+++ connect4.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from enum import EnumMeta
 from typing import Any
 
 
@@ -7,9 +9,23 @@ PLAYER2 = "yellow"
 
 class Connect4:
     def __init__(self) -> None:
+        # board[i][j]が、j行 i列
         self.board = [
-            [None] * 7
-        ] * 6
+            [None] * 6
+        ] * 7
+        self.currentPlayer = PLAYER1
 
     def play(self, msg: dict[str, Any]) -> dict[str, Any]:
-        return {}
+        if msg.get("type") != "play":
+            raise RuntimeError(f"unsupported type: {msg.get('type')}")
+        col = msg["column"]
+        return {
+        }
+
+    def find_bottom(self, col: int) -> int:
+        cells = self.board[col]
+        i = len(cells) - 1
+        for i, c in enumerate(cells):
+            if c is None:
+                return i
+        raise RuntimeError(f"column {col} is fulfilled.")
