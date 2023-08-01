c4d2b9fb18bc3d677e241753716d136b126fae40
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-08-01 11:46:58 +0900

    fix

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 
 PID=$(ps -ef | grep 'python app.py' | grep -v grep | awk '{print $2}')
-if ! [[ -z ${PID} ]]; then
+if ! [[ -z "${PID}" ]]; then
     echo "killing ${PID}"
     kill ${PID}
 else
